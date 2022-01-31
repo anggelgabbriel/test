5262dac375a9a5d8d474edd7545b747afd05a948
Gabriel <anggelgabbriel@hotmail.com>
2022-01-31 02:00:04 -0300

    add admin seeder info

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -15,32 +15,13 @@ class UserSeeder extends Seeder
      */
     public function run()
     {
-        //
-//        $user = new User;
-//        $user->name = 'Romerito GMAIL';
-//        $user->email = 'romerito.campos@gmail.com';
-//        $user->password = \Hash::make('123');
-//        $user->save();
-//
-//        $user = new User;
-//        $user->name = 'Romerito IFRN';
-//        $user->email = 'romerito.campos@ifrn.edu.br';
-//        $user->password = \Hash::make('123');
-//        $user->save();
-//
-//        $user = new User;
-//        $user->name = 'Romerito IFRN';
-//        $user->email = 'romerito.campos@editor';
-//        $user->password = \Hash::make('123');
-//        $user->save();
+        $user = new User;
+        $user->name = 'Romerito GMAIL';
+        $user->email = 'romerito.campos@gmail.com';
+        $user->isadmin = true;
+        $user->password = \Hash::make('123');
+        $user->save();
 
-//        $exam = new Exam;
-//        $exam->user_id = 0;
-//        $exam->type = 0;
-//        $exam->save();
-
-//        User::factory(10)->create();
-//        Exam::factory(10)->create();
         User::factory(10)->create()->each(function ($user) {
             $user->exam()->save(Exam::factory()->make());
         });
